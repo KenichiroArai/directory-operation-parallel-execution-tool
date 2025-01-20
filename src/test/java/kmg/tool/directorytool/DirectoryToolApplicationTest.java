4c061383e49e5742e5108de19c33f088b91d60af
KenichiroArai <sammyleejunior@gmail.com>
2025-01-20 22:59:34 +0900

    DirectoryToolApplicationTestクラスにアプリケーションコンテキストのロード確認テストとメインメソッドの実行テストを追加しました

--- src/test/java/kmg/tool/directorytool/DirectoryToolApplicationTest.java
+++ src/test/java/kmg/tool/directorytool/DirectoryToolApplicationTest.java
@@ -1,6 +1,9 @@
 package kmg.tool.directorytool;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 /**
  * DirectoryToolApplicationのテストクラス。<br>
@@ -8,8 +11,51 @@ import org.springframework.boot.test.context.SpringBootTest;
  * アプリケーションの主要な機能とエラーハンドリングをテストします。
  * </p>
  */
+@SuppressWarnings("static-method")
 @SpringBootTest
 public class DirectoryToolApplicationTest {
 
-    // TODO 未実装
+    /**
+     * アプリケーションコンテキストが正常にロードされることを確認するテスト
+     *
+     * @param context
+     *                自動注入されるアプリケーションコンテキスト
+     */
+    @Test
+    public void contextLoads(final ApplicationContext context) {
+
+        // コンテキストが正常にロードされたことを確認
+        Assertions.assertNotNull(context, "アプリケーションコンテキストが正常にロードされていません");
+
+    }
+
+    /**
+     * メインメソッドが例外をスローせずに正常に実行されることを確認するテスト
+     */
+    @Test
+    public void testMain() {
+
+        Assertions.assertDoesNotThrow(() -> {
+
+            DirectoryToolApplication.main(new String[] {});
+
+        }, "メインメソッドの実行中に例外が発生しました");
+
+    }
+
+    /**
+     * コマンドライン引数を指定した場合のメインメソッド実行テスト
+     */
+    @Test
+    public void testMainWithArguments() {
+
+        Assertions.assertDoesNotThrow(() -> {
+
+            DirectoryToolApplication.main(new String[] {
+                    "--spring.main.banner-mode=off"
+            });
+
+        }, "引数付きメインメソッドの実行中に例外が発生しました");
+
+    }
 }
