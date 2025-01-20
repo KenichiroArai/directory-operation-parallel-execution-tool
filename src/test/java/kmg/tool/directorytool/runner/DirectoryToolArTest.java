c81c32468848fd1fa6d51b570cffdf752778a93f
KenichiroArai <sammyleejunior@gmail.com>
2025-01-20 15:54:26 +0900

    DirectoryToolArTestクラスで、テスト準備時にリストアペンダーのクリア処理を削除しました。

--- src/test/java/kmg/tool/directorytool/runner/DirectoryToolArTest.java
+++ src/test/java/kmg/tool/directorytool/runner/DirectoryToolArTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -29,6 +31,7 @@ import kmg.tool.directorytool.service.DirectoryService;
 /**
  * DirectoryToolArのテストクラス
  */
+@Execution(ExecutionMode.SAME_THREAD)
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class DirectoryToolArTest implements AutoCloseable {
